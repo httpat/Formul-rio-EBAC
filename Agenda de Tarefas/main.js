16ef3bc53d64817e7757590d4b456df02e01730c
httpat <patriciavbalduci@icloud.com>
2023-08-01 19:23:35 -0300

    a

--- Agenda de Tarefas/main.js
+++ Agenda de Tarefas/main.js
@@ -6,10 +6,18 @@ $(document).ready(function(){
     $('#botaoCancelar').click(function(){
         $('form').slideUp();
     })
-   
+
     $('form').on('submit', function(e) {
         e.preventDefault();
+        const valorDaTarefa = $('#tarefa').val();
+        const novaTarefa = $('<li></li>');
 
-        $('<input>').appendTo('li')
-    })
-})
+        $(`<li> ${valorDaTarefa} </li>`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ol');
+        })}
+)
+
+$( 'li' ).on( "click", function() {
+    $( this ).css( "text-decoration", "line-through" );
+  });
+
